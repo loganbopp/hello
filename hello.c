b3b3791f7862852892413e22e02a4a2d85f27a4d
Logan <Logan>
2021-08-26 16:56:50 +0000

    added my to the output

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Logan Bopp John Plewa
+ * Date: 2021/08/26
+ *
+ * A simple hello world program in C
+ *
+ */
+  #include <stdlib.h>
+  #include <stdio.h>
+
+ int main(int argc, char **argv) {
+
+     printf("Computer Science\n");
+     printf("Logan Bopp!\n")
+
+     return 0;
+ }
